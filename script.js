76e916864fbeb17d5cacea6391e1a22fb242b08c
erandadervishi <erandadervishi10@yahoo.com>
2024-04-08 22:31:56 +0200

    update: hamburger menu added

---
+++ script.js
@@ -0,0 +1,15 @@
+let x = document.getElementById("top_nav");
+
+function hamburgerToggle() {
+    x.classList.toggle("open");
+  }
+
+function hello() {
+    alert("This is my portfolio web page!")
+}
+
+let y = document.getElementById("portfolio");
+
+function showMore() {
+    y.classList.toggle("more");
+}
