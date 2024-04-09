655fd78b5850be62372bf7b3dee0387e745817a2
erandadervishi <erandadervishi10@yahoo.com>
2024-04-09 14:54:50 +0200

    update the modal images

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
+// hamburger menu
+
 let x = document.getElementById("top_nav");
 
 function hamburgerToggle() {
     x.classList.toggle("open");
-  }
+}
+
+
+// alert
 
 function hello() {
     alert("This is my portfolio web page!")
 }
 
+// load more..
+
 let y = document.getElementById("portfolio");
 
 function showMore() {
     y.classList.toggle("more");
+}
+
+// modal img
+
+let modal = document.getElementById("myModal");
+
+let images = document.getElementsByClassName('myImages');
+let modalImg = document.getElementById("img01");
+
+for (let i = 0; i < images.length; i++) {
+    let img = images[i];
+
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+    modal.style.display = "none";
 }
